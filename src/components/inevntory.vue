ca721c9d151a6b92fdc0dccb3eb7b7185092c9d7
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-27 20:50:31 +0600

    change card style

--- src/components/inevntory.vue
+++ src/components/inevntory.vue
@@ -2,11 +2,12 @@
   <div class="">
       <div class="row">
          
-            <div class="card border-primary mr-2 col-lg-3 mb-2" v-for="(item, index) in items" :key="index">
-               <img class="card-img-top" :src="item.photo" alt="">
+            <div class="card border-primary mr-2 col-lg-5 mb-2" v-for="(item, index) in items" :key="index">
+               <img class="card-img-top mt-2" :src="item.photo" alt="">
                <div class="card-body">
                <h4 class="card-title">{{ item.title }}</h4>
-               <p class="card-text"></p>
+               <p class="card-text">This is a dummy text for for all div</p>
+                <a href="#" class="btn btn-info">{{ item.price }}</a>
                </div>
             </div>
         
