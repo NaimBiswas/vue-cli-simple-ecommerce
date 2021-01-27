ca17bd598ffead48a28e2f3af6f56bed9b62a2c9
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-27 21:30:53 +0600

    add total pricing list

--- src/components/carft.vue
+++ src/components/carft.vue
@@ -4,10 +4,16 @@
        <tr>
           <td>Name</td>
           <td>Price</td>
+          
+          
        </tr>
       <tr v-for="(item, index) in items" :key="index">
         <td>{{ item.title }}</td>
         <td>{{ item.price }}</td>
+      </tr>
+      <tr>
+         <td class="text-center">Total</td>
+         <td>$120</td>
       </tr>
     </table>
   </div>
