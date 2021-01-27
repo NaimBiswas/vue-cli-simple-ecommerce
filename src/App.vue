73be2a6246c215822e4aced20a26a452eeaf88a4
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-27 19:41:11 +0600

    devided into two section

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
  <div class="">
     
     <nav class="navbar navbar-expand-sm navbar-light bg-warning">
-       <a class="navbar-brand" href="#">Navbar</a>
+       <a class="navbar-brand" href="#">Awesome Store</a>
        <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
            aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -12,14 +12,12 @@
              <li class="nav-item active">
                 <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
              </li>
-             <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-             </li>
+             
              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Contact</a>
                 <div class="dropdown-menu" aria-labelledby="dropdownId">
-                   <a class="dropdown-item" href="#">Action 1</a>
-                   <a class="dropdown-item" href="#">Action 2</a>
+                   <a class="dropdown-item" href="#">Github</a>
+                   <a class="dropdown-item" href="#">LinkedIn</a>
                 </div>
              </li>
           </ul>
@@ -29,7 +27,17 @@
           </form>
        </div>
     </nav>
-
+    <div class="container">
+       <div class="row">
+         <div class="col-lg-8">
+            Intentory
+         </div>
+         <div class="col-lg-4">
+            Card
+         </div>
+      </div>
+    </div>
+    
  </div>
 </template>
 
