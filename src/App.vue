5aee5caa0f690876bf07221bcb51c4d7d61718ef
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-27 19:45:07 +0600

    add card and image

--- src/App.vue
+++ src/App.vue
@@ -27,10 +27,19 @@
           </form>
        </div>
     </nav>
-    <div class="container">
+    <div class="container mt-4">
        <div class="row">
          <div class="col-lg-8">
-            Intentory
+            <div class="row">
+             <div class="card border-primary">
+               <img class="card-img-top" src="https://via.placeholder.com/150
+C/O https://placeholder.com/" alt="">
+               <div class="card-body">
+                 <h4 class="card-title">Title</h4>
+                 <p class="card-text">Text</p>
+               </div>
+             </div>
+            </div>
          </div>
          <div class="col-lg-4">
             Card
