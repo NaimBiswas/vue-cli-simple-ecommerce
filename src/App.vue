e5b6402566b9afd10f6b71f1e67092711a5d396e
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-27 21:11:18 +0600

    sent cart data form app page and revice form carft page

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
         </div>
         <div class="col-lg-4">
            <!-- cart section  -->
-          <carft></carft>
+          <carft :item= 'cart'></carft>
         </div>
       </div>
     </div>
@@ -28,6 +28,7 @@ export default {
   data() {
      return {
         items: [],
+        cart: [],
      }
   },
   components: {
