1bd838441c8bf7aef005ebd40ccd1b2a16e18c0a
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-27 21:15:38 +0600

    add for loop for item add

--- src/components/carft.vue
+++ src/components/carft.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="">
     <table class="table table-bordered">
-      <tr>
-        <td>Name</td>
-        <td>Price</td>
+       <tr>
+          <td>Name</td>
+          <td>Price</td>
+       </tr>
+      <tr v-for="(item, index) in items" :key="index">
+        <td>{{ item.title }}</td>
+        <td>{{ item.price }}</td>
       </tr>
     </table>
   </div>
