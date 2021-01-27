287bd424ae30bd41db876e4f0b5f7f7ac5b38e8d
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-27 18:00:00 +0600

    config bootstrap jquery and popper js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import jquery from 'jquery'
+import bootstrap from 'bootstrap'
+import popper from '@popperjs/core'
+window.jquery = jquery
+window.bootstrap = bootstrap
+window.popper = popper
+
+
 
 createApp(App).mount('#app')
