c50e2b78f5f51d95df70e8c81f8bf1d34f62c3fa
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-27 19:48:31 +0600

    cope card three time and set margin-left

--- src/App.vue
+++ src/App.vue
@@ -31,7 +31,26 @@
        <div class="row">
          <div class="col-lg-8">
             <div class="row">
-             <div class="card border-primary">
+               <!-- Card One  -->
+             <div class="card border-primary mr-2">
+               <img class="card-img-top" src="https://via.placeholder.com/240
+C/O https://placeholder.com/" alt="">
+               <div class="card-body">
+                 <h4 class="card-title">Title</h4>
+                 <p class="card-text">Text</p>
+               </div>
+             </div>
+               <!-- Card One  -->
+             <div class="card border-primary  mr-2">
+               <img class="card-img-top" src="https://via.placeholder.com/240
+C/O https://placeholder.com/" alt="">
+               <div class="card-body">
+                 <h4 class="card-title">Title</h4>
+                 <p class="card-text">Text</p>
+               </div>
+             </div>
+               <!-- Card One  -->
+             <div class="card border-primary  mr-2">
                <img class="card-img-top" src="https://via.placeholder.com/240
 C/O https://placeholder.com/" alt="">
                <div class="card-body">
