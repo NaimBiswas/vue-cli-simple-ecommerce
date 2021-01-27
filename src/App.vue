795b7f0f69308633c0f684cf53880a2a622cc375
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-27 19:20:35 +0600

    create three file

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  
-  
+ <div class="">
+   <about></about>
+ </div>
 </template>
 
 <script>
 
 
+
+
+
+
+
+import About from "./components/about.vue"
 export default {
   name: 'App',
   components: {
-    
+   About
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/about.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="">
+     <h3>This is about test</h3>
+  </div>
+</template>
+
+<script>
+export default {
+   
+
+}
+</script>
+
+<style>
+
+</style>
