95710f90c9d7ad901baf92e452671d0317482136
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-27 20:42:03 +0600

    remove card

--- src/components/inevntory.vue
+++ src/components/inevntory.vue
@@ -10,24 +10,6 @@ C/O https://placeholder.com/" alt="">
                  <p class="card-text">Text</p>
                </div>
              </div>
-               <!-- Card One  -->
-             <div class="card border-primary  mr-2">
-               <img class="card-img-top" src="https://via.placeholder.com/240
-C/O https://placeholder.com/" alt="">
-               <div class="card-body">
-                 <h4 class="card-title">Title</h4>
-                 <p class="card-text">Text</p>
-               </div>
-             </div>
-               <!-- Card One  -->
-             <div class="card border-primary  mr-2">
-               <img class="card-img-top" src="https://via.placeholder.com/240
-C/O https://placeholder.com/" alt="">
-               <div class="card-body">
-                 <h4 class="card-title">Title</h4>
-                 <p class="card-text">Text</p>
-               </div>
-             </div>
             </div>
   </div>
 </template>
