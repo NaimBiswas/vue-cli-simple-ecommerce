a37819f33ed1365b04b99ff02446f69a02eac507
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-27 19:57:30 +0600

    cut inventroy from app and paste it on inventory

---
+++ src/components/indevtory.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="">
+      <div class="row">
+               <!-- Card One  -->
+             <div class="card border-primary mr-2">
+               <img class="card-img-top" src="https://via.placeholder.com/240
+C/O https://placeholder.com/" alt="">
+               <div class="card-body">
+                 <h4 class="card-title">Title</h4>
+                 <p class="card-text">Text</p>
+               </div>
+             </div>
+               <!-- Card One  -->
+             <div class="card border-primary  mr-2">
+               <img class="card-img-top" src="https://via.placeholder.com/240
+C/O https://placeholder.com/" alt="">
+               <div class="card-body">
+                 <h4 class="card-title">Title</h4>
+                 <p class="card-text">Text</p>
+               </div>
+             </div>
+               <!-- Card One  -->
+             <div class="card border-primary  mr-2">
+               <img class="card-img-top" src="https://via.placeholder.com/240
+C/O https://placeholder.com/" alt="">
+               <div class="card-body">
+                 <h4 class="card-title">Title</h4>
+                 <p class="card-text">Text</p>
+               </div>
+             </div>
+            </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
