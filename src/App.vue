42239e51e9d804d496268a3e8587c6c6ea3a5082
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-27 20:01:40 +0600

    set formate

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,33 @@
 <template>
- <div class="">
+  <div class="">
     <navBar></navBar>
-   
+
     <div class="container mt-4">
-       <div class="row">
-         <div class="col-lg-8">
-           <inevntory></inevntory>
-         </div>
-         <div class="col-lg-4">
-           <carft></carft>
-         </div>
+      <div class="row">
+        <div class="col-lg-8">
+          <inevntory></inevntory>
+        </div>
+        <div class="col-lg-4">
+          <carft></carft>
+        </div>
       </div>
     </div>
-    
- </div>
+  </div>
 </template>
 
 <script>
-
-
-
-
-
-import navBar from '@/components/navBar'
-import inevntory from '@/components/inevntory'
-import carft from '@/components/carft'
+import navBar from "@/components/navBar";
+import inevntory from "@/components/inevntory";
+import carft from "@/components/carft";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-   navBar,
-   inevntory,
-   carft
-  }
-}
+    navBar,
+    inevntory,
+    carft,
+  },
+};
 </script>
 
 <style>
-
 </style>
