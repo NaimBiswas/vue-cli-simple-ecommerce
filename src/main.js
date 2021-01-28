f505a70d9bebe8378739c9f291e5bd05d959b3e8
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-28 10:25:42 +0600

    import vue router and config it

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 import jquery from 'jquery'
 import bootstrap from 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Router from 'vue-router'
+Vue.use(Router)
 
 window.jquery = jquery
 window.bootstrap = bootstrap
